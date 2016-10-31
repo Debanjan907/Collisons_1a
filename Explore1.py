5f43c43fcfa630c43c823dc3b940875186f72243
VirileVicissitude <debanjan.907@gmail.com>
2016-10-31 00:39:53 -0400

    minor change

--- Explore1.py
+++ Explore1.py
@@ -17,7 +17,6 @@ from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt;
 import matplotlib.patches as mpatches
 from sklearn.neighbors.kde import KernelDensity
-
 import pandas as pd
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
@@ -319,32 +318,3 @@ def run_kde(city, year, kde):
     # print res
 # run_kde('nyc',2014,kde)
 '''
-
-
-def PoissonRegression(city, year):
-    file = utils.Data_dir + '/' + city + '_' + str(year) + '_' + utils.discreet_str + '.csv'
-    data = pd.read_csv(file)
-    data['count'] = 1
-    data_with_count = data.groupby(['weekday', 'time', 'lat', 'lon']).count()
-    data_with_count = data_with_count.reset_index()
-    del data_with_count['d_i'];
-    del data_with_count['death'];
-    del data_with_count['injury']
-
-    file = utils.Data_dir + '/' + city + '_' + str(year) + '_' + utils.discreet_str + '_neg.csv'
-    all_neg_samples = pd.read_csv(file)
-    all_neg_samples['count'] = 0;
-    del all_neg_samples['d_i'];
-    del all_neg_samples['death'];
-    del all_neg_samples['injury']
-
-    combined = all_neg_samples.merge(data_with_count, how='left', on=['weekday', 'time', 'lat', 'lon']).fillna(0)
-    del combined['count_x']
-    combined.rename(columns={'count_y': 'count'}, inplace=True)
-    formula = 'count ~ weekday + time + lat + lon';
-    model = smf.glm(formula = formula, data = combined, family = sm.families.Poisson()).fit()
-    print (model.summary())
-    print (model.predict(pd.DataFrame( { 'weekday' : [3], 'time':[8], 'lat':[40.5], 'lon':[-74.2]})))
-    return;
-
-PoissonRegression('nyc', 2013)
